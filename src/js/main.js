a7d27e558919c07bae51bddde74c18583fe57483
vikassangwan003 <vikassangwan003@gmail.com>
2018-10-21 22:10:01 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -178,7 +178,7 @@ const createRestaurantHTML = (restaurant) => {
   image.alt=`Picture of ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
